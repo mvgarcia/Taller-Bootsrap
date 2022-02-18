e67740f429c3ed9e1dd9931b0cf16c3b1a2be7d3
mvgarcia <mv.garcia@uniandes.edu.co>
2022-02-18 15:44:16 -0500

    Reorganizar archivos

---
+++ Ejercicio javascript/ejercicio4.js
@@ -0,0 +1,30 @@
+let secret = (a,f,cl)=>{
+    if(f==='encrypt'){
+        return a.map(a=>{return a+cl});
+    }
+    else if(f==='decrypt'){
+        return a.map(a=>{return a-cl});
+    }
+}
+
+let mcd = (a,b)=>{
+    if(a==b){
+        return a;
+    }
+    else if(a>b){
+        return mcd(b,a-b);
+    }
+    return mcd(a,b-a);
+}
+
+let fibonacci = (n)=>{
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    return(fibonacci(n-2)+fibonacci(n-1))
+}
+
+console.log(fibonacci(1))
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Taller-Bootsrap
-Taller Bootstrap para la clase desarrollo web
+Talleres para la clase desarrollo web
+
+El taller de bootstrap está en la carpeta "Ejercicio boostrap"
+
+El ejercicio de javascript está en la carpeta "Ejercicio javascript"
