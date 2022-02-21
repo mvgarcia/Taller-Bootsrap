998f34c2a86a9138c5239a4c23971032577fbdb0
mvgarcia <mv.garcia@uniandes.edu.co>
2022-02-21 18:01:27 -0500

    Cambiar funciones a una sola linea

--- Ejercicio javascript/ejercicio4.js
+++ Ejercicio javascript/ejercicio4.js
@@ -1,13 +1,15 @@
-let secret = (a,f,cl)=>{
+/* let secret = (a,f,cl)=>{
     if(f==='encrypt'){
         return a.map(a=>{return a+cl});
     }
     else if(f==='decrypt'){
         return a.map(a=>{return a-cl});
     }
-}
+} */
 
-let mcd = (a,b)=>{
+let secret = (a,f,cl)=> f==='encrypt'? a.map(a=>{return a+cl}) : (f==='decrypt'? a.map(a=>{return a-cl}) : "error")
+
+/* let mcd = (a,b)=>{
     if(a==b){
         return a;
     }
@@ -15,9 +17,11 @@ let mcd = (a,b)=>{
         return mcd(b,a-b);
     }
     return mcd(a,b-a);
-}
+} */
 
-let fibonacci = (n)=>{
+let mcd = (a,b)=>(a==b)? a : (a>b ? mcd(b,a-b) : mcd(a,b-a))
+
+/* let fibonacci = (n)=>{
     if(n==0){
         return 0;
     }
@@ -25,4 +29,6 @@ let fibonacci = (n)=>{
         return 1;
     }
     return(fibonacci(n-2)+fibonacci(n-1))
-}
+} */
+
+let fibonacci = (n)=> n==0? 0 : (n==1? 1 : fibonacci(n-2)+fibonacci(n-1))
